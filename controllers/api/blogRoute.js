616caba91db9e541a633892c90074ecd61e1ffe8
Pema Sherpa <pematamang111@gmail.com>
2024-04-15 15:20:38 -0400

    add update route

--- controllers/api/blogRoute.js
+++ controllers/api/blogRoute.js
@@ -15,6 +15,19 @@ router.post('/', withAuth, async (req, res) => {
 }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updateBlog = await Blog.update(req.params.id, {
+            where: req.params.id,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(updateBlog);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
 router.delete('/:id', withAuth, async (req, res) => {
     try {
        const blogData = await Blog.destroy({
