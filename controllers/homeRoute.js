f80bfb0731cefd59d6edfd26141d6e33ca155731
Pema Sherpa <pematamang111@gmail.com>
2024-04-14 21:46:04 -0400

    add home routes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -31,6 +31,34 @@ router.get('/blog/:id', withAuth, async (req, res) => {
         ...blog,
         logged_in: req.session.logged_in
      });
-     
+
 } catch (err) { res.status(400).json(err) }
 })
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+    const profileOfId = await User.findByPk(req.session.user_id, {
+       attributes: [ { exclude: 'password' }],
+       include: [{ model: Blog }],
+    })
+
+    const profile = profileOfId.get({ plain: true });
+
+    res.render('profile', {
+        ...profile,
+        logged_in: true,
+    });
+
+} catch (err) {res.status(400).json(err) }
+})
+
+router.get('/login', withAuth, async (req, res) => {
+if(req.session.logged_in){
+    res.redirect('/profile');
+    return;
+}
+res.render('login');
+});
+
+module.exports = router;
+
