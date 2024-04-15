494e2501ec36cc840f5ebffb61dabc54a1cce33f
Pema Sherpa <pematamang111@gmail.com>
2024-04-15 11:04:51 -0400

    add express-session typos fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //dependencies
 const express = require('express');
 const path = require('path');
-const session = require('session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
